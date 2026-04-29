dc6a67437753ca05cedf03f956b7d981806d8d93
Sujal Dwivedi <sujaldwivedi65@gmail.com>
2026-04-29 03:23:42 +0000

    varable start

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "sujaldwivedi65@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+// accountId = 2 // not allowed
+accountEmail = "thesujaldwivedi@gmail.com"
+console.log(accountId);
