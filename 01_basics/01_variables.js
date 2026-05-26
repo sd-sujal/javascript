8b4fc33870e8c0e7942e30ec499f6bb1d408b163
Sujal Dwivedi <sujaldwivedi65@gmail.com>
2026-05-26 06:25:57 +0000

    11:55AM-(26-05-26)

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,8 +2,23 @@ const accountId = 144553
 let accountEmail = "sujaldwivedi65@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
-
+let accountState;
 
 // accountId = 2 // not allowed
 accountEmail = "thesujaldwivedi@gmail.com"
 console.log(accountId);
+accountPassword =  "0802020"
+accountCity = "Rewa"
+// const value cannot be change in the memory 
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+//var-ignores the scope, allows redecalaration, not recommanded to use today
+//let- use this 
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+//any variable which is declared but not defined - in javascript its value is considered as "underdefined"
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,41 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+//code readability should be high 
+console.log("sujal");
+//ECMA is the institution who defines the standard of write code in javascript or it is the original documentation of javascript
+//Tc39 
+//MDN also provides documentation which is much easier to understand 
+
+//globalobjects,datetime 
+
+
+let name = "sujal"  // string datatype 
+let age = 21 // number
+let isLoggedIn = false // boolean
+let place = null
+
+//primitive datatype
+// number -> 2 ro power 53
+// bigiint
+// string -> ""
+// boolean -> true/false
+// null -> standaline value 'it is a representation of empty value.'
+// undefined -> value is not being assigned yet 
+// symbol ->unique (used in react)
+
+
+//typeof null is object and typeof undefined is undefined 
+
+// Object 
+
+
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
+
+
