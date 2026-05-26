41ecdfc179424880868540623aaa2fe6003dc4ad
Sujal Dwivedi <sujaldwivedi65@gmail.com>
2026-05-26 17:05:56 +0000

    completed V-7

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = undefined
 
-console.log(typeof score);
-console.log(typeof(score));//method type
+// console.log(typeof score);
+// console.log(typeof(score));//method type
 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);//NaN- Not a Number 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//NaN- Not a Number 
 
 
 
@@ -18,7 +18,7 @@ console.log(valueInNumber);//NaN- Not a Number
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1-> true; 0->false
 // "" ->false
@@ -27,7 +27,47 @@ console.log(booleanIsLoggedIn);
 let num = 241030021
 
 let stringnum = String(num)
-console.log(stringnum);
+// console.log(stringnum);
 
-console.log(typeof stringnum);
+// console.log(typeof stringnum);
+
+// *******************************Operations ***************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Sujal"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1"+2)//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+"2"+3);//123
+// console.log(1+7+"2"+3);//823
+// console.log(1+2+"2");//32
+
+// console.log(true); // true
+// console.log(+true);// 1
+// console.log(+"");//0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+3
+
+console.log(num1);
+console.log(num2);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
+
 
