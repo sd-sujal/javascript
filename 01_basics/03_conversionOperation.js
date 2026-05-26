c8103debde1cf968fbe61cac47a25f4974891343
Sujal Dwivedi <sujaldwivedi65@gmail.com>
2026-05-26 15:50:21 +0000

    conversion of data type 9:19PM-(26-06-26)

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score));//method type
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//NaN- Not a Number 
+
+
+
+// "33"->33
+// "33abc"->NaN not a number
+// true -> 1
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1-> true; 0->false
+// "" ->false
+// "sujal" ->true
+
+let num = 241030021
+
+let stringnum = String(num)
+console.log(stringnum);
+
+console.log(typeof stringnum);
+
