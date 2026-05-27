b48d6c572ef814dac68fb98141d98d09f2d39ebd
Sujal Dwivedi <sujaldwivedi65@gmail.com>
2026-05-27 17:06:27 +0000

    middle

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,19 @@
+const name = "Sujal"
+const repoCount = 89
+
+// console.log(name + " " + repoCount + " Value");
+
+// String interpolation
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}` );
+
+// Another way to initialise any string is 
+
+const gameName = new String("Sujal")
+
+console.log(gameName);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
