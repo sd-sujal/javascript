b95704ffd895f1e9b89c3e64d272f0d191e594ba
Sujal Dwivedi <sujaldwivedi65@gmail.com>
2026-05-28 14:09:40 +0000

    V 11 Done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,15 +5,40 @@ const repoCount = 89
 
 // String interpolation
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}` );
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}` );
 
 // Another way to initialise any string is 
 
-const gameName = new String("Sujal")
+const gameName = new String("Sujal-82atkinsTT")
 
-console.log(gameName);
-console.log(gameName.__proto__);
+// console.log(gameName);
+// console.log(gameName.__proto__);
 
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('S'));
+
+// const newString = gameName.slice(-8,4)// only in .slice() you can use negative index
+// console.log(newString);
+
+// const anotherString = gameName.substring(0,4);
+// console.log(anotherString);
+// //trim and replace 
+// const newStringOne = "    i am a second third year student    "// trimm works only in line terminatators and whitespace   
+// console.log(newStringOne);
+
+// console.log(newStringOne.trim());
+
+// const url = " https://Sujal.com/Sujal%20Dwivedi"
+
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('Sujal'));
+
+//to convert a string into array based on anything like space or '-'
+
+let text = "How are you doing today?";
+const myArray = text.split(" ");
+console.log(myArray);
