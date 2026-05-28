8f3b284ada31f140721b2552db00995aacc8de3b
Sujal Dwivedi <sujaldwivedi65@gmail.com>
2026-05-28 15:33:09 +0000

    V 12 Done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+// const score = 400 
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber=1123.89233
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.max(4,5,3,5,3,2,78,43,23,98));
+// console.log(Math.min(4,5,3,5,3,2,78,43,23,98));
+
+console.log(Math.random());// value between 0-1
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);// to get value between (1,10)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min ); // Important formula 
+
+
