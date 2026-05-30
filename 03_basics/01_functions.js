ec6aa4bc7084d82eba57e07793ba4be7068e4298
sd-sujal <sujaldwivedi65@gmail.com>
2026-05-30 20:33:01 +0530

    V 20 Done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,7 +33,36 @@ function loginuserMessage(username = "deafult value"){
     return `${username} just logged in.`
 }
 
-console.log(loginuserMessage("Sujal"));
-console.log(loginuserMessage()); // gives undefined 
+// console.log(loginuserMessage("Sujal"));
+// console.log(loginuserMessage()); // gives undefined 
 
+function calculateCartPrice(va1,val2,...num1){// '...' rest operator used to pass multiple value in a function
+    return num1
+}
+// console.log(calculateCartPrice(8,9,2,7,6));
 
+const user = {
+    username: "sujal",
+    price:  199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is
+        ${anyobject.price} `);
+    
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "jaideep",
+    price: 399
+})
+
+const Arr = [100, 200, 300, 400]
+
+function returnSeconfValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSeconfValue(Arr));
