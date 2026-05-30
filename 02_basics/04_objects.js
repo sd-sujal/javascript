da6f66d796792860112ea2d5012c1cc75b4f8ef4
sd-sujal <sujaldwivedi65@gmail.com>
2026-05-30 18:16:37 +0530

    V 17 Done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,4 +26,38 @@ const regularUser = {
 //optional chaining console.log(regularUser.fullname.?userfullname);
 
 const obj1 = {1: "a", 2: "b"}
-const obj1 = {1: "a", 2: "b"}
+const obj4 = {99: "a", 22: "b"}
+const obj5 = {6: "a", 21: "b"}
+const obj2 = {3: "c", 4: "d"}
+// const obj3 = { obj1, obj2}// { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4, obj5)// see the documentation 
+// Object.assign() static method copies all enumerable own properties frm one ore source objects to a 
+// target objects. It returns the modified target object.
+
+const obj3 = {...obj1, ...obj2, ...obj4} //cmost commonly used (using spread operator)
+// console.log(obj3);
+
+const user = [
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+     {
+        id: 2,
+        email: "xyz@gmail.com"
+    }, {
+        id: 3,
+        email: "kbc@gmail.com"
+    }
+]
+
+user[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
