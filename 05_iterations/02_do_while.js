dccbe8d117bd8aea1bd2dfb572d746008f94454f
sd-sujal <sujaldwivedi65@gmail.com>
2026-05-31 21:03:00 +0530

    V 28 Done

---
+++ 05_iterations/02_do_while.js
@@ -0,0 +1,21 @@
+
+// let index = 0
+// while (index<=10) {
+//     console.log(`value of index is ${index}`)
+//     index=index+2
+// }
+
+// let arr=['cristiano ronaldo', 'mbappe', 'messi', 'ramos', 'marcelo']
+// let i=0
+// while (i<arr.length) {
+//     console.log(`player is ${arr[i]} at ${i+1}`)
+//     i++;
+// }
+
+// let score=11
+
+// do{
+//     console.log(`Score is ${score}`);
+//     score++
+    
+// }while(score<=10)
