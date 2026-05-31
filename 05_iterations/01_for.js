9d93242447271331b5861f00d14ecd42c98a8c0a
sd-sujal <sujaldwivedi65@gmail.com>
2026-05-31 20:41:56 +0530

    V 27 Done

---
+++ 05_iterations/01_for.js
@@ -0,0 +1,45 @@
+// for 
+
+// for(let i=0;i<10;i++){
+//     const element = i;
+//     if (element == 5 ){
+//        // console.log("5 is best number");
+//     }
+//     // console.log(element);
+// }
+
+// for(let i=1;i<=5;i++){
+//     console.log(`Outer loop value: ${i}`)
+//     for(let j=1;j<=5;j++){
+//         //console.log(`inner loop: ${j}`)
+//         console.log(i+ '*' + j + ' = ' + i*j);
+//     }
+// }
+
+
+// let arr = ["flash", "greenlaten", "homelander"]
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// break and continnue
+
+// for (let index = 1; index < 20; index++) {
+//     if (index == 5) {
+//         console.log(`value detected: ${index}`)
+//         break
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+for (let index = 1; index < 7; index++) {
+    if (index == 5) {
+        console.log(`value detected: ${index}`)
+        continue
+    }
+    console.log(`value of i is ${index}`);
+    
+}
+
+
